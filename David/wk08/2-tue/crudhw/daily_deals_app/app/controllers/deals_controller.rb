5168f01ad15911350b8beb570032971bd8ee1598
David McDeavitt <davidmcdeavitt@Davids-MacBook-Air.local>
2017-07-19 09:37:37 +1000

    add crud before database

---
+++ David/wk08/2-tue/crudhw/daily_deals_app/app/controllers/deals_controller.rb
@@ -0,0 +1,25 @@
+class DealsController < ApplicationController
+
+  def index
+    @deals = Deal.all
+
+  end
+
+  def new
+  end
+
+  def create
+    dish = Deal.new
+    dish.name = params[:name]
+    dish.image_url = params[:image_url]
+    dish.save
+    redirect_to '/dishes'
+  end
+
+
+
+
+
+
+
+end
